d7da907ebde7807200a8de0b08579f9cc3b266ba
Nourhane <n.boudaoud@my-groom-service.com>
2024-02-20 09:16:58 +0100

    feature: limite de pdf par user

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,20 +6,41 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
-
+use App\Repository\PDFRepository;
 
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
-    public function index(UserInterface $username): Response
+    private $pdfRepository;
+
+    public function __construct(PDFRepository $pdfRepository)
     {
-        $username = $this->getUser()->getFirstname();
-        $subscription = $this->getUser()->getSubs()->getTitle();
+        $this->pdfRepository = $pdfRepository;
+    }
+
+    #[Route('/profile', name: 'app_profile')]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+        $username = $user->getFirstname();
+        $subscription = $user->getSubs();
+        $subscriptionTitle = $subscription->getTitle();
+        $pdfLimit = $subscription->getLimitsPdf();
+        
+        // Get the count of PDFs made today by the user
+        $startOfDay = new \DateTime('today');
+        $endOfDay = new \DateTime('tomorrow');
+        $pdfCountToday = $this->pdfRepository->countPdfGeneratedByUserOnDate($user->getId(), $startOfDay, $endOfDay);
+
+        // Check if the user has exceeded the PDF limit
+        $pdfLimitExceeded = $pdfCountToday >= $pdfLimit;
 
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
             'username' => $username,
-            'subscription' => $subscription,
+            'subscription_title' => $subscriptionTitle,
+            'pdf_count_today' => $pdfCountToday,
+            'pdf_limit' => $pdfLimit,
+            'pdf_limit_exceeded' => $pdfLimitExceeded,
         ]);
     }
 }
--- src/Service/GotenbergService.php
+++ src/Service/GotenbergService.php
@@ -2,26 +2,34 @@
 
 namespace App\Service;
 
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\Exception\{
+    TransportExceptionInterface,
+    ClientExceptionInterface,
+    RedirectionExceptionInterface,
+    ServerExceptionInterface
+};
 
 class GotenbergService
 {
     private HttpClientInterface $client;
+    private ParameterBagInterface $params;
 
-    public function __construct(HttpClientInterface $client)
+    public function __construct(HttpClientInterface $client, ParameterBagInterface $params)
     {
         $this->client = $client;
+        $this->params = $params;
     }
 
     /**
-
-    @throws TransportExceptionInterface
-    @throws ServerExceptionInterface
-    @throws RedirectionExceptionInterface
-    @throws ClientExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
      */
-    public function generatePdfFromUrl(string $url): string{
-
+    public function generatePdfFromUrl(string $url, string $pdfName): string
+    {
         $htmlContent = file_get_contents($url);
 
         $response = $this->client->request(
@@ -34,7 +42,8 @@ class GotenbergService
                 'body' => [
                     'url' => $url,
                 ]
-            ]);
+            ]
+        );
 
         if ($response->getStatusCode() !== 200) {
             // Gérer l'erreur ici
@@ -44,8 +53,23 @@ class GotenbergService
         // Récupérer le contenu du PDF généré
         $pdfContent = $response->getContent();
 
+        // Obtenir le chemin du dossier "uploads"
+        $uploadsDirectory = $this->params->get('kernel.project_dir') . '/public/uploads';
+
+        // Vérifier si le répertoire "uploads" existe et s'il est accessible en écriture
+        if (!is_dir($uploadsDirectory) || !is_writable($uploadsDirectory)) {
+            throw new \Exception('Uploads directory is not accessible or writable.');
+        }
+
+        // Nommer le fichier PDF
+        $pdfFileName = $pdfName . '.pdf';
+
+        // Chemin complet du fichier PDF
+        $pdfFilePath = $uploadsDirectory . '/' . $pdfFileName;
+
+        $filepath ='/public/uploads/' . $pdfName . '.pdf';
+
         // Sauvegarder le contenu dans un fichier PDF
-        $pdfFilePath = sys_get_temp_dir() . '/' . uniqid('generated_pdf') . '.pdf';
         file_put_contents($pdfFilePath, $pdfContent);
 
         // Retourner le chemin du fichier PDF généré
